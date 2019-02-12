c503ff839649d15fc857110d367409183260a09b
YurleySolimer <yurley.solimer@gmail.com>
2019-02-12 18:32:05 -0400

    get tasks

--- app.js
+++ app.js
@@ -9,6 +9,36 @@ fuction saveTask (e) {
 		description
 	};
 
+	if (localStorage.getItem ("tasks") == null ) {
+		let tasks = [] ;
+		tasks.push (task);
+		localStorage.setItem ( "tasks", JSON.stringify (tasks));
+	}
 
+	else {
+		let tasks = JSON.parse (localStorage.getItem ("tasks"));
+		tasks.push (task);
+		localStorage.setItem ("tasks", JSON.stringify (tasks));
+	}
 	e.preventDefault();
 } 
+
+fuction getTask () {
+	let tasks = JSON.parse (localStorage.getItem ("tasks"));
+	let tasksView = document.getElementById ("tasks");
+
+	tasksView.innerHTML = "" ;
+
+	for (let i = 0; i < tasks.length; i++) {
+		let title = tasks [i].title;
+		let description = tasks [i].description;
+		
+		tasksView.innerHTML += `<div class="card mb-3">
+        <div class="card-body">
+          <p>${title} - ${description}
+          <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">Delete</a>
+          </p>
+        </div>
+      </div>`;
+	}
+}
